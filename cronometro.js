b164939dab3861af78fe5ba14de069e4f6a69746
natalia vieira <verkappte@hotmail.com>
2023-03-31 10:46:45 -0300

    primeiro funcionamento do cronômetro

--- cronometro.js
+++ cronometro.js
@@ -1,1 +1,40 @@
-!
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const milisecondsEl = document.querySelector("#miliseconds");
+const startBtnEl = document.querySelector("#startBtn");
+const pauseEl = document.querySelector("#pauseBtn");
+const resumeEl = document.querySelector("#resumeBtn");
+const resetEl = document.querySelector("#resetBtn");
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let miliseconds =0;
+let isPaused= false;
+
+startBtnEl.addEventListener("click", startTimer)
+
+function startTimer() {
+    interval = setInterval(() => {
+
+        if(!isPaused) {
+
+            miliseconds += 10;
+        }
+
+        if(miliseconds === 1000){
+            seconds++;
+            miliseconds = 0;
+        }
+
+        if(seconds === 60){
+            minutes++;
+            seconds = 0;
+        }
+
+        minutesEl.textContent = minutes;
+        secondsEl.textContent = seconds;
+        milisecondsEl.textContent = miliseconds;
+    }, 10);
+}
+
